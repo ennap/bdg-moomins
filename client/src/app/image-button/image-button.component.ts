98bc6471ba79d2bc90ce808f254ace0bd6b63e9e
Enna Phan <ennap@uci.edu>
2024-06-10 21:41:40 -0700

    flicker transition on match winners

--- client/src/app/image-button/image-button.component.ts
+++ client/src/app/image-button/image-button.component.ts
@@ -1,13 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-button',
   templateUrl: './image-button.component.html',
   styleUrls: ['./image-button.component.scss']
 })
-export class ImageButtonComponent{
+export class ImageButtonComponent implements OnChanges{
   @Input() src:string = "../assets/empty.png";
+  @Input() img: string = "../assets/empty.png";
   @Input() x:string = "0";
   @Input() y:string = "0";
   @Input() size:string = "130";
+  @Input() flicker:boolean = true;
+
+  sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+  async increment() {
+    await this.sleep(500);
+    this.img = this.src;
+    await this.sleep(300);
+    this.img = "../assets/empty.png";
+    await this.sleep(300);
+    this.img = this.src;
+    await this.sleep(300);
+    this.img = "../assets/empty.png";
+    await this.sleep(300);
+    this.img = this.src;
+  };
+
+  ngOnChanges(): void{
+    if (this.flicker) {
+      this.increment();
+    }
+    else {
+      this.img = this.src;
+    }
+  }
 }
